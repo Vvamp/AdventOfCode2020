c42a1b57d057aa04ac891406102307a2ac5a827a
= <vvampus@gmail.com>
2020-12-02 20:45:53 +0100

    Finished day 2

--- Day2/1/main.py
+++ Day2/1/main.py
@@ -2,17 +2,22 @@ a = open('input.txt', 'r')
 input = a.readlines()
 a.close()
 
-goal = 2020
-print(input)
+validCount = 0
 for line in input: 
-    for line2 in input:
-        line = line.replace('\n', '') 
-        line2 = line2.replace('\n', '')
-        #print("{}, {}".format(line, line2))
-        if line == '' or line2 == '':
-            continue
-#        print("Result: {}".format(int(line) + int(line2)))
-        if int(line) + int(line2) == goal: 
-            print("RESULT: {} * {} = {}".format(line, line2, int(line)*int(line2)))
-            exit(0)
-print("No results found. Probabl error in code")
+    line = line.replace('\n', '')
+    if line == '' or line == None:
+        break
+    numbers = line.split(' ')[0]
+    minimumCount = int(numbers.split('-')[0])
+    maximumCount = int(numbers.split('-')[1])
+    characterToMatch = line.split(' ')[1].replace(':', '') 
+    body = line.split(' ')[2] 
+    charCount = 0
+    for char in body: 
+        if char == characterToMatch:
+            charCount += 1 
+    if charCount >= minimumCount and charCount <= maximumCount:
+        validCount += 1 
+    
+
+print("Valid Passwords: {}".format(validCount))
--- Day2/2/main.py
+++ Day2/2/main.py
@@ -2,18 +2,24 @@ a = open('input.txt', 'r')
 input = a.readlines()
 a.close()
 
-goal = 2020
-print(input)
+validCount = 0
 for line in input: 
-    for line2 in input:
-        for line3 in input:
-            line = line.replace('\n', '') 
-            line2 = line2.replace('\n', '')
-            line3 = line3.replace('\n', '') 
-            if line == '' or line2 == '' or line3 == '':
-                continue
-#        print("Result: {}".format(int(line) + int(line2)))
-            if int(line) + int(line2) + int(line3) == goal: 
-                print("RESULT: {} * {} * {} = {}".format(line, line2, line3, int(line)*int(line2)*int(line3)))
-                exit(0)
-print("No results found. Probabl error in code")
+    line = line.replace('\n', '')
+    if line == '' or line == None:
+        break
+    numbers = line.split(' ')[0]
+    firstNumIndex = int(numbers.split('-')[0])-1 # +1 because index 0 is 1
+    secondNumIndex = int(numbers.split('-')[1])-1 
+    characterToMatch = line.split(' ')[1].replace(':', '') 
+    body = line.split(' ')[2]
+    
+    first = False
+    second = False 
+    if characterToMatch == body[firstNumIndex]:
+        first = True 
+    if characterToMatch == body[secondNumIndex]:
+        second= True 
+
+    if first != second: 
+        validCount += 1
+print("Valid Passwords: {}".format(validCount))
